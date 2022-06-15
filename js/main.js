c617ba200ffa2eacf78e88640fa164beda515333
PhuocMinh111 <phuocminhlenguyen@gmail.com>
2022-06-15 08:18:19 +0700

    4 commit

--- js/main.js
+++ js/main.js
@@ -1,5 +1,30 @@
+import Service from "./models/service.js";
 function getEle(id) {
-    return document.getElementById(id)
+  return document.getElementById(id);
 }
 const cart = document.createElement("span");
-cart.innerHTML = ``
+const s = new Service();
+function getProducts() {
+  s.get()
+    .then((res) => {
+      const data = res.data;
+      console.log(data);
+      renderProducts(data);
+    })
+    .catch((err) => {});
+}
+function renderProducts(data) {
+  const content = data.reduce((_, item) => {
+    const { img, desc, type, name } = item;
+    _ += `
+        <div class="card">
+        <img src="${img}" alt="phone"/>
+        <h4 class="name">${name}</h4>
+        <p class="desc">${desc} </p>
+        </div>
+        `;
+    return _;
+  }, "");
+  getEle("products").innerHTML = content;
+}
+getProducts();
--- js/models/service.js
+++ js/models/service.js
@@ -1,12 +1,12 @@
-const URL = ""
-export default class Product {
-    constructor(list) {
-        this.list = list;
-    }
-    get() {
-        return axios({
-            url: URL,
-            method: "get"
-        })
-    }
+const URL = "https://62a8907aec36bf40bda8f061.mockapi.io/PhoneList";
+export default class Service {
+  constructor(list) {
+    this.list = list;
+  }
+  get() {
+    return axios({
+      url: URL,
+      method: "get",
+    });
+  }
 }
