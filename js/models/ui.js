2282612e8a5f83647afac08b79288dab3ab24072
PhuocMinh111 <phuocminhlenguyen@gmail.com>
2022-06-22 18:51:22 +0700

    fix modal

--- js/models/ui.js
+++ js/models/ui.js
@@ -1,64 +1,72 @@
 export default class UI {
-    // constructor(data) {
-    //     this.data = data;
-    // }
-    render(data) {
-
-    }
-    getEle(id) {
-        return document.getElementById(id)
-    }
-    renderProducts(data) {
-        const content = data.reduce((_, item) => {
-            const { img, id, desc, type, price, name } = item;
-            const ID = Number.parseInt(id);
-            _ += `
+  // constructor(data) {
+  //     this.data = data;
+  // }
+  render(data) {}
+  getEle(id) {
+    return document.getElementById(id);
+  }
+  renderProducts(data) {
+    const content = data.reduce((_, item) => {
+      const { img, id, desc, type, price, name } = item;
+      const ID = Number.parseInt(id);
+      _ += `
         <div  class="card">
         <img src="${img}" alt="phone"/>
         <div class="footer text-light">
         <h4 class="name">${name}</h4>
         <p class="desc">${desc} </p>
         <div class="addToCart">
-        <span class="price">${price}</span>
-        <button id="${ID}" data-toggle="modal" data-target="#modelId" class="btn btn-light toCart" >add</button>
+        <span class="price">${price} $</span>
+        <button id="${ID}"  class="btn btn-light toCart" >add</button>
         </div>
         </div>
         </div>
         `;
-            return _;
-        }, "");
-        this.getEle("products").innerHTML = content;
-    }
+      return _;
+    }, "");
+    this.getEle("products").innerHTML = content;
+  }
 
-
-
-    renderCart(cartList) {
-        const content = cartList.reduce((_, item) => {
-            const { img, name, price } = item;
-            console.log(img);
-            _ += `
+  renderCart(cartList) {
+    const content = cartList.reduce((_, item) => {
+      const { img, name, price } = item;
+      _ += `
             <li class="mt-3">
             <img src="${img}" style="width:50px" />
             <span>${name} </span>
             </li>
         `;
-            return _;
-        }, "");
-        const contentQuantity = cartList.reduce((_, item) => {
-            const { quantity, id } = item;
-            console.log(quantity);
-            _ += `
+      return _;
+    }, "");
+    const contentQuantity = cartList.reduce((_, item) => {
+      const { quantity, id } = item;
+      console.log(quantity);
+      _ += `
             <li id="${id}" class="quantity mt-3">
-            <span class="down text-primary"> </span>
             <h5 class="">${quantity}</h5>
-            <span class="up text-primary"><i class="fas fa-plus"></i></span>
-            </li>`
-            return _;
-        }, "")
-        this.getEle("prod-show").innerHTML = content;
-        this.getEle("prod-quantity").innerHTML = contentQuantity;
+            <button id="del" class="btn ml-3 btn-danger">del</button>
+            </li>`;
+      return _;
+    }, "");
+    const numberProd = cartList.reduce((_, item) => {
+      const { quantity } = item;
+      _ += quantity;
+      return _;
+    }, 0);
+    const totalPrice = cartList.reduce((_, item) => {
+      const { price, quantity } = item;
+      _ += parseInt(price) * parseInt(quantity);
+      //   const finalPrice = arr.find((item) => item != "$");
 
-    }
-
-
+      //   return finalPrice;
+      return _;
+    }, 0);
+    console.log(totalPrice);
+    console.log(numberProd);
+    this.getEle("total-price").innerHTML = `${totalPrice} $`;
+    this.getEle("prod-show").innerHTML = content;
+    this.getEle("prod-quantity").innerHTML = contentQuantity;
+    this.getEle("cart-number").innerHTML = numberProd;
+  }
 }
